ef166f3272caa6f060af48f911a0b0030c4baddc
Maksim Andronov <oopd2017@gmail.com>
2020-07-16 14:17:40 +0500

    added react-quiz

---
+++ src/axios/axios-quiz.js
@@ -0,0 +1,5 @@
+import axios from 'axios'
+
+export default axios.create({
+    baseURL: 'https://react-data-c5739.firebaseio.com'
+})
---
+++ src/components/UI/Select/Select.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import classes from './Select.module.css'
+
+const Select = props => {
+
+  const htmlFor = `${props.label}-${Math.random()}`
+    
+  return (
+      <div className={classes.Select}>
+        <label htmlFor={htmlFor}></label>  
+        <select
+          id={htmlFor}
+          value={props.value}
+          onChange={props.onChange}
+        >
+            {
+                props.options.map((option, index) => {
+                    return (
+                      <option
+                        key={option.value + index}
+                        value={option.value}
+                      >
+                        {option.value} 
+                      </option>
+                    )
+                })
+            }
+        </select>
+      </div>
+  )
+}
+
+export default Select
